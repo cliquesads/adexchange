bcfcd868ebd1324a6c1ee51b92f643589108e42d
Ben Liang <bliang@cliquesads.com>
2015-02-18 18:56:21 -0500

    NF: Working test auction!

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
 var express = require('express');
-var math = require('mathjs');
 var fs = require('fs');
-var http = require('http');
 var br = require('./lib/bid_requests');
-
+var querystring = require('querystring');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+
+app.set('port', (process.env.PORT || 5100));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
+    response.send('Welcome to the Cliques Ad Exchange');
 });
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-app.get('/exchange/bids', function(request, response) {
-    response.send('<h1>This is an exchange</h1><p>Dont really know what im doing</p>');
-});
+function generate_test_bid_urls(num_urls){
+    // temporary function to generate bunch of test bid URLs
+    var base_url = "http://localhost:5000/bid?";
+    var urls = [];
+    for (var i = 0; i < num_urls; i++) {
+        var query = {
+            "request_id": Math.round(Math.random() * 10e7),
+            "bidder_id": Math.round(Math.random() * 10e2)
+        };
+        urls.push(base_url + querystring.encode(query));
+    }
+    return urls;
+}
 
 app.get('/exchange/test_auction', function(request, response){
-    response.send(main(request, response))
+    // Test runs & vars
+    var bid_urls = generate_test_bid_urls(10);
+    var winning_bid = {};
+    br.get_bids(bid_urls, function(err, result){
+        if (err) throw err;
+        winning_bid = br.run_auction(result, function(err, winning_bid){
+            response.status(200).json(winning_bid).send('<')
+        });
+    });
 });
-
-
-
-function main(request, response){
-    var bids;
-}
 
 
 
