2715584f27a6eba107a702e5335657bbfa1db297
Ben Liang <bliang@cliquesads.com>
2015-02-17 22:53:06 -0500

    NF: Lots of changes, but mainly added bid_requests.js after finally getting async.parallel to work

---
+++ dummy.js
@@ -0,0 +1,3 @@
+/**
+ * Created by bliang on 2/17/15.
+ */
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 var express = require('express');
+var math = require('mathjs');
+var fs = require('fs');
+var http = require('http');
+var br = require('./lib/bid_requests');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+app.get('/exchange/bids', function(request, response) {
+    response.send('<h1>This is an exchange</h1><p>Dont really know what im doing</p>');
+});
+
+app.get('/exchange/test_auction', function(request, response){
+    response.send(main(request, response))
+});
+
+
+
+function main(request, response){
+    var bids;
+}
+
+
+
+
