ef477e70f0a191990340a20ac8f7192c664b938d
Ben Liang <bliang@cliquesads.com>
2015-02-25 14:54:54 -0500

    Change back to port 5000

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(function(req, res, next) {
 });
 app.use(cookieParser());
 app.use(responseTime());
-app.set('port', (process.env.PORT || 5100));
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // custom cookie-parsing middleware
