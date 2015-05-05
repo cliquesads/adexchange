cfb447b962b485f890a6a146cce1f1ed62fe73ae
Ben Liang <bliang@cliquesads.com>
2015-05-05 14:07:01 -0400

    NF: Add setup scripts for redis source install

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -4,11 +4,18 @@
 sudo apt-get update
 sudo apt-get install gcc make build-essential
 
+#clone config repo and make symlink
+if [ ! -d $HOME"/repositories/cliques-config" ]; then
+    git clone git@github.com:cliquesads/cliques-config.git ../cliques-config
+    ln -s ../cliques-config config
+fi
+
+./setup-redis.sh
+
 #download NVM and install NVM & node
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | NVM_DIR=$HOME/repositories/adexchange/.nvm bash
 source .nvm/nvm.sh
 nvm install 0.12.0
-nvm use 0.12.0
 
 source ./activate_production.sh
 
@@ -19,8 +26,3 @@ npm install
 npm install pm2 -g
 npm install mocha -g
 
-#clone config repo and make symlink
-if [ ! -d $HOME"/repositories/cliques-config" ]; then
-    git clone git@github.com:cliquesads/cliques-config.git ../cliques-config
-    ln -s ../cliques-config config
-fi
