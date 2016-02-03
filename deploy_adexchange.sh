3201f73435cf7f7c3c586240bae6578622d8b3e5
Ben Liang <bliang@cliquesads.com>
2016-02-03 17:01:05 -0500

    NF: Update $HOSTNAME var in deploy script

--- deploy_adexchange.sh
+++ deploy_adexchange.sh
@@ -69,7 +69,7 @@ running=$(pm2 list -m | grep "$processname")
 
 if [ -z "$running" ]; then
     # hook PM2 up to web monitoring with KeyMetrics
-    pm2 link k79qp6h0w795o48 l6aqtb33eaiqyry $hostname
+    pm2 link k79qp6h0w795o48 l6aqtb33eaiqyry $HOSTNAME
     # start in cluster mode
     pm2 start index.js --name "$processname" -i 0
 else
