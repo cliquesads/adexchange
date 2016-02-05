3aedaa954484f73241854abfb1d22b73a0421e0c
Ben Liang <bliang@cliquesads.com>
2016-02-05 13:30:40 -0500

    Try adding environment upgrades back into activate scripts

--- activate_dev.sh
+++ activate_dev.sh
@@ -8,7 +8,6 @@ export NODE_ENV=dev
 alias redis-cli=$HOME/redis-3.0.1/src/redis-cli
 
 #have to point to the right version of node, npm, pm2, mocha
-nvm use 0.12.0
-#nvm use 5.0
-#npm install pm2@1.0.0 -g
-#pm2 update
+nvm install 5.0
+npm install pm2@1.0.0 -g
+pm2 update
