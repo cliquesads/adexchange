3055ba80cac047e618f0e15a5e43e9cb78db84f3
Ben Liang <bliang@cliquesads.com>
2017-09-24 17:35:53 -0400

    Fix unittests to test for new multi-impression, multi-auction scenarios

--- test/test.js
+++ test/test.js
@@ -8,7 +8,8 @@ process.env.NODE_ENV = 'test';
 
 // have to set port before importing or it will try to spin up app on 5000
 process.env['EXCHANGE-WEBSERVER-PORT'] = 5200;
-var index = require('../index');
+var logger = require('../lib/logger');
+
 
 describe('MongoDB', function(){
     it('Can connect to database: exchange', function(done){
@@ -41,6 +42,7 @@ describe('Config', function(){
 describe('WebServer', function(){
     //UUID value on incoming request gets propagated to request object for downstream processing
     //var req = {cookies: {uuid: "cc820770-c1e6-11e4-b7ba-e977f4853d86"}};
+    var index = require('../index');
     var app = index.app;
     describe('GET /', function() {
         it("Responds with 200", function (done) {
@@ -62,7 +64,7 @@ describe('Auctioneer', function(){
 
     //var bidders = config.get('Exchange.bidders');
     var timeout = config.get('Exchange.bidder_timeout');
-    var auctioneer = new br.Auctioneer({}, timeout, index.devNullLogger);
+    var auctioneer = new br.Auctioneer({}, timeout, logger.devNullLogger);
 
     describe('MultiSeatBids', function(){
         it("Should return highest bid as the winner, with clearprice of second highest bid + $0.01", function () {
@@ -72,6 +74,8 @@ describe('Auctioneer', function(){
                     "seat": 8433,
                     "bid": [
                         {"id": 5597483, "impid": "230429219812962", "price": 5.94},
+                        {"id": 55397343, "impid": "230429219812270", "price": 4.24}, // test different impids as well
+                        {"id": 55397347, "impid": "230429219812270", "price": 7.92}, // test different impids as well
                         {"id": 95885061, "impid": "230429219812962", "price": 2.75},
                         {"id": 41452679, "impid": "230429219812962", "price": 1.02},
                         {"id": 54945329, "impid": "230429219812962", "price": 2.3},
@@ -82,14 +86,19 @@ describe('Auctioneer', function(){
                 }]
                 }
             ];
-            auctioneer.runAuction({},test_bids_single_obj, function (err, winning_bid) {
+            var bids = br.flattenBidObjects(test_bids_single_obj);
+            auctioneer.runAuction({}, bids[230429219812962], function(err, winning_bid){
                 assert.equal(winning_bid.id, 68110451);
                 assert.equal(winning_bid.clearprice, 5.95);
+            });
+            auctioneer.runAuction({},bids[230429219812270], function (err, winning_bid) {
+                assert.equal(winning_bid.id, 55397347);
+                assert.equal(winning_bid.clearprice, 4.25);
             });
         });
     });
     describe('MultiBidObjects', function(){
-        it("Should return highest bid as the winner, with clearprice of second highest bid + $0.01", function () {
+        it("Should return highest bid for each impression as the winner, with clearprice of second highest bid + $0.01", function () {
             var test_bids_multi_obj = [
                 {"id": "268bd720-c528-11e4-8524-6fd57e6c05e5","bidid": 54156237771,"cur": "USD","seatbid":
                     [{"seat": 8475,"bid": [{"id": 86422736,"impid": "231159286491452","price": 3.95}]}]
@@ -109,13 +118,26 @@ describe('Auctioneer', function(){
                 {"id": "268bd720-c528-11e4-8524-6fd57e6c05e5","bidid": 21633179206,"cur": "USD","seatbid":
                     [{"seat": 9744,"bid": [{"id": 47271633,"impid": "231159286491452","price": 3.18}]}]
                 },
+                {"id": "268bd720-c528-11e4-8524-6fd57e6c05e5","bidid": 21633172873,"cur": "USD","seatbid":
+                    [{"seat": 974,"bid": [{"id": 47368292,"impid": "211159286491762","price": 2.11}]}]
+                },
+                {"id": "268bd720-c528-11e4-8524-6fd57e6c05e5","bidid": 37928839928,"cur": "USD","seatbid":
+                    [{"seat": 972,"bid": [{"id": 38929028,"impid": "211159286491762","price": 5.01}]}]
+                },
                 {"id": "268bd720-c528-11e4-8524-6fd57e6c05e5","bidid": 31455981801,"cur": "USD","seatbid":
                     [{"seat": 8583,"bid": [{"id": 48705300,"impid": "231159286491452","price": 6.58}]}]
                 }
             ];
-            auctioneer.runAuction({},test_bids_multi_obj, function (err, winning_bid){
+            var bids = br.flattenBidObjects(test_bids_multi_obj);
+
+            auctioneer.runAuction({},bids[231159286491452], function (err, winning_bid) {
                 assert.equal(winning_bid.id, 40577572);
                 assert.equal(winning_bid.clearprice, 6.59);
+            });
+
+            auctioneer.runAuction({},bids[211159286491762], function (err, winning_bid) {
+                assert.equal(winning_bid.id, 38929028);
+                assert.equal(winning_bid.clearprice, 2.12);
             });
         });
     });
@@ -124,14 +146,15 @@ describe('Auctioneer', function(){
 describe('BottomUpAuctioneer', function(){
 
     var timeout = config.get('Exchange.bidder_timeout');
-    var auctioneer = new br.BottomUpAuctioneer({}, timeout, index.devNullLogger);
+    var auctioneer = new br.BottomUpAuctioneer({}, timeout, logger.devNullLogger);
     var test_bids = [
         {
             "id": "7367a940-c526-11e4-9d22-a5269bd04333", "bidid": 2029191609, "cur": "USD", "seatbid": [
             {
                 "seat": "Bottom Clique",
                 "bid": [
-                    {"id": 5597483, "impid": "230429219812962", "price": 5.94}
+                    {"id": 5597483, "impid": "230429219812962", "price": 5.94},
+                    {"id": 5592803, "impid": "230429219372902", "price": 2.25}
                 ]
             },
             {
@@ -142,7 +165,9 @@ describe('BottomUpAuctioneer', function(){
             },
             {
                 "seat": "Two Levels Up",
-                "bid": []
+                "bid": [
+                    {"id": 55923881, "impid": "230429219372902", "price": 4.25}
+                ]
             },
             {
                 "seat": "Three Levels Up",
@@ -173,18 +198,26 @@ describe('BottomUpAuctioneer', function(){
         }
     };
 
+    var new_bids = br.flattenBidObjects(test_bids);
     describe('Bottom Up Auction', function(){
         it("Bid Request from Bottom placement should return winner from Bottom Clique ", function () {
-
-            auctioneer.runAuction(bottom_placement,test_bids, function (err, winning_bid) {
+            auctioneer.runAuction(bottom_placement,new_bids[230429219812962], function (err, winning_bid) {
                 assert.equal(winning_bid.id, 5597483);
                 assert.equal(winning_bid.clearprice, 5.94);
             });
+            auctioneer.runAuction(bottom_placement,new_bids[230429219372902], function (err, winning_bid) {
+                assert.equal(winning_bid.id, 5592803);
+                assert.equal(winning_bid.clearprice, 2.25);
+            });
         });
         it("Bid Request from two-up placement should return winner from third-up, if two-up clique has no bids", function () {
-            auctioneer.runAuction(two_up_placement,test_bids, function (err, winning_bid) {
+            auctioneer.runAuction(two_up_placement,new_bids[230429219812962], function (err, winning_bid) {
                 assert.equal(winning_bid.id, 55974833297);
                 assert.equal(winning_bid.clearprice, 5.95);
+            });
+            auctioneer.runAuction(two_up_placement,new_bids[230429219372902], function (err, winning_bid) {
+                assert.equal(winning_bid.id, 55923881);
+                assert.equal(winning_bid.clearprice, 4.25);
             });
         });
     });
