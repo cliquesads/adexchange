a1c70b71b5145d7a064d9d35182d81d4978ad2cd
Ben Liang <bliang@cliquesads.com>
2017-12-05 23:43:07 -0500

    refactor to get ride of jade & replace w/ pug

--- test/test.controller.js
+++ test/test.controller.js
@@ -1,6 +1,6 @@
 var config = require('config');
 var querystring = require('querystring');
-var jade = require('jade');
+var pug = require('pug');
 var TEST_PLACEMENTS = config.get("Exchange.test.placement");
 
 module.exports = function(publisherModels, auctioneer){
@@ -15,7 +15,7 @@ module.exports = function(publisherModels, auctioneer){
             publisherModels.getNestedObjectById(request.query.pid, 'Placement', function (err, placement) {
                 if (err) logger.error(err);
                 auctioneer._create_bid_request(placement, request, function (err, request_data) {
-                    var fn = jade.compileFile('./templates/rtb_test.jade', null);
+                    var fn = pug.compileFile('./templates/rtb_test.pug', null);
                     var html = fn({request_data: JSON.stringify(request_data, null, 2), qs: qs});
                     response.send(html);
                 });
@@ -27,7 +27,7 @@ module.exports = function(publisherModels, auctioneer){
             //     var rendered1 = pubTag.render(placement1);
                 publisherModels.getNestedObjectById(TEST_PLACEMENTS.multiUnitNative.id, 'Placement', function (err, placement2) {
                     if (err) console.log(err);
-                    var fn = jade.compileFile('./templates/test_ad.jade', null);
+                    var fn = pug.compileFile('./templates/test_ad.pug', null);
                     var rendered2 = pubTag.render(placement2);
                     var html = fn({pubtag2: rendered2, pid2: placement2.id });
                     response.send(html);
