509ff33904c73274882a57b5fa9ef8d816283169
Ben Liang <bliang@cliquesads.com>
2017-12-10 22:39:15 -0500

    Add wrapper script for logs extractor to make it easier to run in cron

---
+++ logsextractor.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# usage text visible when --help flag passed in
+usage="$(basename "$0") -- Wrapper script for node-utils logsextractor, which moves log files to cloud storage and
+deletes them
+
+where:
+    --help  show this help text
+    -p path/to/pm2/logfiles
+    -w path/to/winston/logfiles"
+
+# BEGIN arg parsing
+
+if [ ! -z $1 ]; then
+  if [ $1 == '--help' ]; then
+    echo "$usage"
+    exit 0
+  fi
+fi
+
+pm2=''
+winston=''
+
+# fucking getopts
+while getopts ":p:" opt; do
+  case $opt in
+    e)
+      pm2="$OPTARG"
+      ;;
+#    \?)
+#      echo "Invalid option: -$OPTARG" >&2
+#      echo "$usage"
+#      exit 1
+#      ;;
+#    :)
+#      echo "Environment flag -$OPTARG requires an argument (either 'dev' or 'production')" >&2
+#      exit 1
+#      ;;
+  esac
+done
+
+while getopts ":w:" opt; do
+  case $opt in
+    w)
+      winston="$OPTARG"
+      ;;
+#    \?)
+#      echo "Invalid option: -$OPTARG" >&2
+#      echo "$usage"
+#      exit 1
+#      ;;
+#    :)
+#      echo "Environment flag -$OPTARG requires an argument (either 'dev' or 'production')" >&2
+#      exit 1
+#      ;;
+  esac
+done
+
+source activate_env.sh
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+node node_modules/@cliques/cliques-node-utils/logsextractor.js -p $pm2 -w $winston
+
+exit 0
