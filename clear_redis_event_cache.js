05d79809484e137e778314965577dfd51bd8ddda
Ben Liang <bliang@cliquesads.com>
2018-01-15 22:11:51 -0500

    BF: Forgot to parameterize datasets in clear redis cache script

--- clear_redis_event_cache.js
+++ clear_redis_event_cache.js
@@ -2,17 +2,14 @@
  * Created by bliang on 6/6/15.
  */
 var node_utils = require('@cliques/cliques-node-utils');
+var config = require('config');
 var transports = node_utils.transports;
 var bigQueryUtils = node_utils.google.bigQueryUtils;
 var googleAuth = node_utils.google.auth;
 
-
-if (process.env.NODE_ENV === 'production'){
-    var bq_config = bigQueryUtils.loadFullBigQueryConfig('./bq_config.json');
-} else {
-    // use dev config if not running in production
-    bq_config = bigQueryUtils.loadFullBigQueryConfig('./bq_config_dev.json','/google/bq_config_dev.json');
-}
+const adEventDataset = config.get("Exchange.logger.bigQuery.adEventDataset");
+const httpEventDataset = config.get("Exchange.logger.bigQuery.httpEventDataset");
+var bq_config = bigQueryUtils.loadFullBigQueryConfig('./bq_config.json', httpEventDataset, adEventDataset);
 var eventStreamer = new bigQueryUtils.BigQueryEventStreamer(bq_config,
     googleAuth.DEFAULT_JWT_SECRETS_FILE,5);
 var redisEventCache = new transports.RedisEventCache({ eventStreamer: eventStreamer});
